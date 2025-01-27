b45f684636a4335eacad00809e5a18f8d03a17d3
Gauresh Raghav Gururaj <ggururaj@uci.edu>
2025-01-26 17:38:49 -0800

    Add comments and main function

--- PartA.py
+++ PartA.py
@@ -1,20 +1,28 @@
+# O(n) time complexity where n is num of words in the text since each word is only processed once
 def tokenize(text_file_path):
     tokens = []
+    # Try opening file
     try:
+        # Open file
         file = open(text_file_path, 'r')
         for line in file:
+            # Split each line into words by the spaces
             words = line.split()
             for word in words:
+                # Check if it is an actual alphanumeric token if it is add it to the list
                 if word.isalnum():
                     tokens.append(word.lower())
         file.close()
+    # If you can't find the file print error
     except FileNotFoundError:
         print(f"=The file was not found.")
         return tokens
     return tokens
 
+# For loop iterates over all tokens once so time complexity is O(n)
 def computeWordFrequencies(tokens):
     counts = {}
+    # For each token if it is in the dictionary add 1 to the count if not add it to the dictionary with count 1
     for token in tokens:
         if token in counts:
             counts[token] += 1
@@ -22,13 +30,21 @@ def computeWordFrequencies(tokens):
             counts[token] = 1
     return counts
 
+#Overall O(nlogn) since sort function is O(nlogn) and it dominates the other steps
 def print_counts(counts):
+    # Convert the counts dictionary into a list of tuples O(n) time complexity since each token is converted once
     items = list(counts.items())
 
+    # item[1] is the count for the token and item[0] is the word itself
+    # This is a helper function that sorts count in a decreasing order and the word alphabetically in increasing order
     def sort_alphabet_and_count(item):
         return -item[1], item[0]
 
+    # Sort the list
+    # Built in sorting algorithm has O(nlogn)
     items.sort(key=sort_alphabet_and_count)
 
+    # Print the contents of the sorted list
+    # O(n) since you visit each token once to print it
     for token, count in items:
         print(f"{token} -> {count}")
--- PartB.py
+++ PartB.py
@@ -1,5 +1,27 @@
+import sys
+import PartA
+
+#The runtime complexity is O(n1 + n2) where n1 and n2 are the sizes for tokens1 and 2
+#This is because converting a list of size n to a set takes O(n) time complexity
 def countCommonTokens(tokens1, tokens2):
+    #Convert to sets so it's easier to find intersection
     set1 = set(tokens1)
     set2 = set(tokens2)
+    #find intersection time complexity is O(min(n1,n2))
     return len(set1 & set2)
 
+def main():
+    # Get the file paths from the command-line arguments
+    file1 = sys.argv[1]
+    file2 = sys.argv[2]
+
+    # Use tokenize to get tokens of both files
+    file1_tokens = PartA.tokenize(file1)
+    file2_tokens = PartA.tokenize(file2)
+
+    # Find shared tokens using countCommonTokens then print the number
+    shared_tokens = countCommonTokens(file1_tokens, file2_tokens)
+    print(shared_tokens)
+
+if __name__ == "__main__":
+    main()
